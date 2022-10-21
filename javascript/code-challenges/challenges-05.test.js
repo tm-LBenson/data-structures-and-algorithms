c1ce4b89ed94a790812b04cc71c3fcd006465fd2
Lewis Benson <tm.lewis.benson@gmail.com>
2022-10-21 15:48:37 -0400

    complete challenges 1-7 with passing tests

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -11,11 +11,7 @@ should convert to ["Jane Doe", "James Bond"]
 Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
-const toLastNames = people => {
-  return people.map((person) => {
-    return `${person.firstName} ${person.lastName}`;
-  });
-};
+const toLastNames = people => people.map(person => `${person.firstName} ${person.lastName}`);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
