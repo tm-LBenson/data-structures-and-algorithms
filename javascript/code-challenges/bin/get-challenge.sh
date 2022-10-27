12ff9a2eeb5b3c56240e13d484baa78d94c4b6d6
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-27 10:41:03 -0400

    finish challenges 1-6

--- javascript/code-challenges/bin/get-challenge.sh
+++ javascript/code-challenges/bin/get-challenge.sh
@@ -3,3 +3,5 @@
 echo "Getting Code Challenge $1"
 
 `which curl` -s https://codefellows.github.io/code-301-guide/curriculum/class-$1/challenges/challenges-$1.test.js > ./code-challenges/challenges-$1.test.js
+
+
