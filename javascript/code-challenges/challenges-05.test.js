fd28d1fc14ae698709391af0add99406113a2db6
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-14 13:08:10 -0500

    pass all tests in challenge 5

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -202,7 +202,7 @@ Hint: The accumulator should begin as { count: 0, sum: 0 }
 ------------------------------------------------------------------------------------------------ */
 
 const calculateAverage = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, curr) => curr + acc, 0) / arr.length;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -223,7 +223,7 @@ const isPrime = (value) => {
 };
 
 const countPrimeNumbers = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, cur) => isPrime(cur) ? acc + 1 : acc, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -266,7 +266,13 @@ const snorlaxData = {
 };
 
 const extractStat = (statName, arr) => {
-  // Solution code here...
+  return arr.reduce((acc, cur) => {
+
+    if (cur.stat.name === statName) {
+      acc = cur;
+    }
+    return acc;
+  }, null);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -280,7 +286,13 @@ Write a function named extractChildren that, given the array of characters from 
 ------------------------------------------------------------------------------------------------ */
 
 const extractChildren = (arr) => {
-  // Solution code here...
+
+  const filtered = arr.filter(person => person.name.toLowerCase().includes('a'));
+  return filtered.reduce((acc, cur) => {
+    if (cur.children) acc.push(cur.children);
+    console.log(acc);
+    return acc.flat();
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -343,25 +355,25 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return the average of the numbers in the array', () => {
     expect(calculateAverage([18, 290, 37, 4, 55, 16, 7, 85])).toStrictEqual(64);
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should return a count of the prime numbers in the array', () => {
     expect(countPrimeNumbers([1, 2, 13, 64, 45, 56, 17, 8])).toStrictEqual(3);
   });
 });
 
-xdescribe('Testing challenge 10', () => {
+describe('Testing challenge 10', () => {
   test('It should return any stats that match the input', () => {
     expect(extractStat('speed', snorlaxData.stats)).toStrictEqual({ stat: { url: 'https://pokeapi.co/api/v2/stat/6/', name: 'speed' }, effort: 5, baseStat: 30 });
   });
 });
 
-xdescribe('Testing challenge 11', () => {
+describe('Testing challenge 11', () => {
   test('It should return an array containing the names of the children', () => {
     expect(extractChildren(characters)).toStrictEqual(['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon', 'Drogon', 'Rhaegal', 'Viserion', 'Margaery', 'Loras']);
     expect(extractChildren(characters).length).toStrictEqual(10);
