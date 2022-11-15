f446ba33562f690b4cff5c668be9aa0dcca30f53
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-14 19:52:50 -0500

    pass all tests in challenge 8

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -152,8 +152,11 @@ If the user enters any of these four inputs, return true. For any other input, r
 Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
+
 const matchMonth = (input) => {
-  // Solution code here...
+  if (input.length === 3) return /^([O-o]ct)$/g.test(input);
+  if (input.length === 7) return /^([O-o]ctober)$/g.test(input);
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -167,7 +170,12 @@ The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "
 ------------------------------------------------------------------------------------------------ */
 
 const noPunctuation = str => {
-  // Solution code here...
+
+  const filtered = str.split(' ').filter(word => {
+    return !/(\w+)[,]|(\w+)[!]|(\w+)[?]|(\w+)[\.]/.test(word);
+
+  });
+  return filtered.map(word => word + ' ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -183,7 +191,7 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  return str.replaceAll(/[aeiouAEIOU]/g, '_');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -199,7 +207,8 @@ Hint: All of these words end with the letters "ells".
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
 const findShells = (str) => {
-  // Solution code here...
+  const removePuncuation = str.split('.').join('').split(',').join('');
+  return removePuncuation.split(' ').filter(word => /ells$/g.test(word));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -287,7 +296,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should match any of the acceptable inputs', () => {
     expect(matchMonth('Oct')).toBeTruthy();
     expect(matchMonth('oct')).toBeTruthy();
@@ -305,7 +314,7 @@ xdescribe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia vel massa sed egestas. Nunc faucibus iaculis elit, a scelerisque enim condimentum sed. Aenean ac scelerisque sem, et pharetra diam.';
 
   test('It should only return words that are immediately followed by a space', () => {
@@ -319,7 +328,7 @@ xdescribe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   let startString = 'This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!';
 
   test('It should remove the vowels from the hangman string and replace them with underscores', () => {
@@ -332,7 +341,7 @@ xdescribe('Testing challenge 9', () => {
   });
 });
 
-xdescribe('Testing challenge 10', () => {
+describe('Testing challenge 10', () => {
   test('It should return an array of instances of "sells", shells", and "seashells"', () => {
     expect(findShells(seashells)).toStrictEqual(['sells', 'seashells', 'shells', 'sells', 'seashells', 'sells', 'shells', 'sells', 'shells']);
     expect(findShells(seashells).length).toStrictEqual(9);
