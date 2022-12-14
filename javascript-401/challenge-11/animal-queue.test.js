9e8f2db6e9f6ebea8e8def873a88e3c7a903c9ed
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-13 21:40:05 -0500

    write tests for animal queues

--- javascript-401/challenge-11/animal-queue.test.js
+++ javascript-401/challenge-11/animal-queue.test.js
@@ -5,25 +5,72 @@ const { AnimalQueue } = require('./animal-queue');
 
 const queue = new AnimalQueue();
 
+function animalBuilder(species, value) {
+  return {
+    species: species,
+    value: value
+  };
+}
+
+describe('Animal shelter testing', () => {
+  it('Can successfully instantiate an empty queue', () => {
+    expect(queue.catFront).toBeNull();
+    expect(queue.dogFront).toBeNull();
+  });
+
+  it('Can successfully enqueue multiple values into a queue', () => {
+
+    queue.enqueue(animalBuilder('cat', 'test1'));
+    queue.enqueue(animalBuilder('cat', 'test2'));
+    queue.enqueue(animalBuilder('cat', 'test3'));
+    queue.enqueue(animalBuilder('dog', 'test4'));
+    queue.enqueue(animalBuilder('dog', 'test5'));
+    queue.enqueue(animalBuilder('dog', 'test6'));
+
+    expect(queue.catFront.value.value).toEqual('test1');
+    expect(queue.catFront.next.value.value).toEqual('test2');
+    expect(queue.catFront.next.next.value.value).toEqual('test3');
+    expect(queue.dogFront.value.value).toEqual('test4');
+    expect(queue.dogFront.next.value.value).toEqual('test5');
+    expect(queue.dogFront.next.next.value.value).toEqual('test6');
+  });
+
+  it('Can successfully dequeue out of a queue the expected value', () => {
+    expect(queue.dequeue('cat')).toEqual('test1');
+    expect(queue.dequeue('dog')).toEqual('test4');
+    expect(queue.dequeue('dog')).toEqual('test5');
+    expect(queue.dequeue('dog')).toEqual('test6');
+    expect(queue.dequeue('cat')).toEqual('test2');
+    expect(queue.dequeue('cat')).toEqual('test3');
+  });
+  it('Can successfully empty a queue after multiple dequeues', () => {
+    expect(queue.dogFront).toBeNull();
+    expect(queue.catFront).toBeNull();
+  });
+
+  it('returns null if no pref is passed', () => {
+    expect(queue.dequeue()).toBeNull();
+
+  });
+
+  it('returns null if no pref is passed', () => {
+    queue.enqueue(animalBuilder('cat', 'test1'));
+    expect(queue.dequeue('dog')).toBeNull();
+  });
+
+});
+
+//
+
+//
+
+//
+
+//
 
 
-// 'Can successfully instantiate an empty queue'
 
-// 'Can successfully enqueue multiple values into a queue'
-
-// 'Can successfully dequeue out of a queue the expected value'
-
-// 'Can successfully empty a queue after multiple dequeues'
-
-// 'Can successfully return a 'cat' when the pref 'cat' is passed'
-
-// 'Can successfully return a 'dog' when the pref 'dog' is passed'
-
-// 'Does not return a 'dog' when pref 'cat' is passed'
-
-// 'Does not return a 'cat when pref 'dog' is passed'
-
-// 'returns null if no pref is passed'
+//
 
 // 'returns null if no animal match is found'
 
