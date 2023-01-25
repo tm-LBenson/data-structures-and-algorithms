80cef5cc0fdb29895e39459bd8a1ac83b2068a01
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-24 23:21:00 -0500

    fix linter errors

--- javascript/401/FizzBuzzTreeList/fizzBuzz.test.js
+++ javascript/401/FizzBuzzTreeList/fizzBuzz.test.js
@@ -1,14 +1,14 @@
-"use strict";
-const { BinaryTree } = require("../challenge-15/tree");
-const fizzBuzzList = require("./fizzBuzz");
-describe("fizzBuzzList", () => {
+'use strict';
+const { BinaryTree } = require('../challenge-15/tree');
+const fizzBuzzList = require('./fizzBuzz');
+describe('fizzBuzzList', () => {
   let tree;
 
   beforeEach(() => {
     tree = new BinaryTree();
   });
 
-  test("should return correct linked list for tree with values 1-2-3-4-5", () => {
+  test('should return correct linked list for tree with values 1-2-3-4-5', () => {
     tree.add(1);
     tree.add(2);
     tree.add(3);
@@ -19,30 +19,30 @@ describe("fizzBuzzList", () => {
       next: {
         value: 2,
         next: {
-          value: "Fizz",
-          next: { value: 4, next: { value: "Buzz", next: {} } },
+          value: 'Fizz',
+          next: { value: 4, next: { value: 'Buzz', next: {} } },
         },
       },
     });
   });
 
-  test("should return correct linked list", () => {
+  test('should return correct linked list', () => {
     tree.add(1);
     tree.add(2);
     tree.add(5);
     tree.add(9);
     tree.add(15);
     tree.add(20);
-    expect(fizzBuzzList(tree)).toHaveProperty("next");
+    expect(fizzBuzzList(tree)).toHaveProperty('next');
   });
 
-  test("should return correct linked list for tree with values 15-45-60", () => {
+  test('should return correct linked list for tree with values 15-45-60', () => {
     tree.add(15);
     tree.add(45);
     tree.add(60);
     expect(fizzBuzzList(tree)).toEqual({
-      value: "FizzBuzz",
-      next: { value: "FizzBuzz", next: { value: "FizzBuzz", next: {} } },
+      value: 'FizzBuzz',
+      next: { value: 'FizzBuzz', next: { value: 'FizzBuzz', next: {} } },
     });
   });
 });
